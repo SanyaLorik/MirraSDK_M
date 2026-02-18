153d1f9a11f7e5c4f6cf1054ba368000dd6bda19
Александр Бирюков <112517746+SanyaLorik@users.noreply.github.com>
2026-02-18 23:21:27 +0300

    Add device

---
+++ Device/Scripts/DeviceTypeProviderMirra.cs
@@ -0,0 +1,10 @@
+﻿using Architecture_M;
+using MirraGames.SDK;
+
+namespace MirraSDK_M
+{
+    public class DeviceTypeProviderMirra : DeviceTypeProviderBase
+    {
+        public override DeviceTypeEnum DeviceType => MirraSDK.Device.IsMobile == true ? DeviceTypeEnum.Mobile : DeviceTypeEnum.Desktop;
+    }
+}
