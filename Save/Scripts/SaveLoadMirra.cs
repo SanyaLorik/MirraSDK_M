f70d04ea6baf98b684fabbe8fa13371814b16a7f
Александр Бирюков <112517746+SanyaLorik@users.noreply.github.com>
2026-03-22 00:44:20 +0300

    Update full save

--- Save/Scripts/SaveLoadMirra.cs
+++ Save/Scripts/SaveLoadMirra.cs
@@ -1,24 +1,24 @@
 using Architecture_M;
 using MirraGames.SDK;
+using System;
 using UnityEngine;
 
 namespace MirraSDK_M
 {
-    public class SaveLoadMirra : SaveLoaderBase<GameSaveBase>
+    public class SaveLoadMirra : SaveLoaderBase
     {
         private const string MirraPrefsKey = "GameSave";
 
-        public override GameSaveBase Load()
+        public override object Load(Type type)
         {
-            string defaultData = JsonUtility.ToJson(new GameSaveBase());
+            string defaultData = JsonUtility.ToJson(new object());
             string stringValue = MirraSDK.Data.GetString(key: MirraPrefsKey, defaultValue: defaultData);
 
             Debug.Log($"{MirraPrefsKey} Load {stringValue}");
-            return JsonUtility.FromJson<GameSaveBase>(stringValue);
-            GameSave a = (GameSave)(new GameSaveBase());
+            return JsonUtility.FromJson(stringValue, type);
         }
 
-        public override void Save(GameSaveBase gameSave)
+        public override void Save(object gameSave, Type type)
         {
             string data = JsonUtility.ToJson(gameSave, true);
             MirraSDK.Data.SetString(key: MirraPrefsKey, writeValue: data, important: true);
