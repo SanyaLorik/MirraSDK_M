7c773ef1e5b2582a989b89a89d6623a218d314f8
Александр Бирюков <112517746+SanyaLorik@users.noreply.github.com>
2026-05-11 19:28:42 +0300

    Add rate game

---
+++ SocialNetwork/RateGame/RateGameMirra.cs
@@ -0,0 +1,13 @@
+﻿using Architecture_M;
+using MirraGames.SDK;
+
+namespace MirraSDK_M
+{
+    public class RateGameMirra : RateGameBase
+    {
+        public override void Rate()
+        {
+            MirraSDK.Platform.RateGame();
+        }
+    }
+}
