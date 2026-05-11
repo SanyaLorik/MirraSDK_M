6d1f20499d8d11b124a64873a8b40d25c8aa3127
Александр Бирюков <112517746+SanyaLorik@users.noreply.github.com>
2026-05-11 19:14:46 +0300

    Add leaderboard

---
+++ Player/Scripts/Loaderboard/LeaderboardServiceMirra.cs
@@ -0,0 +1,33 @@
+﻿using Architecture_M;
+using MirraGames.SDK;
+using System.Collections.Generic;
+
+namespace MirraSDK_M
+{
+    partial class LeaderboardServiceMirra : LeaderboardServiceBase
+    {
+        private const int countPlayers = 50;
+
+        private List<PlayerScore> _playerScores = new(countPlayers);
+
+        public override IReadOnlyList<PlayerScore> GetPlayerScores(string leaderboardId)
+        {
+            MirraSDK.Achievements.GetLeaderboard(leaderboardId, (leaderboard) =>
+            {
+                int length = leaderboard.players.Length;
+                for (int i = 0; i < length; i++)
+                {
+                    ref MirraGames.SDK.Common.PlayerScore playerScore = ref leaderboard.players[i];
+                    _playerScores[i] = new(playerScore.displayName, playerScore.position, playerScore.score);
+                }
+            });
+
+            return _playerScores;
+        }
+
+        public override void SetScore(string leaderboardId, int score)
+        {
+            MirraSDK.Achievements.SetScore(leaderboardId, score);
+        }
+    }
+}
